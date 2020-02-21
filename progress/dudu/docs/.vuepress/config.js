dbf51edc709408ebb36c97bae9ce2a9bfa7a22cc
joel <iamjoel007@gmail.com>
2020-02-21 23:00:30 +0800

    add dudu progress

---
+++ progress/dudu/docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  title: '嘟嘟英语学习进度',
+  description: '',
+  base: '/english-learn/progress/dudu/dist/', // 部署站点的基础路径
+  dest: './dist', // 构建输出的位置，从项目根路径开始算。
+  plugins: ['@vuepress/back-to-top'],
+  themeConfig: {
+    // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E5%AF%BC%E8%88%AA%E6%A0%8F
+    nav: [
+      {text: 'GitHub', link: 'https://github.com/iamjoel/basic-skill'}
+    ],
+    // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E4%BE%A7%E8%BE%B9%E6%A0%8F
+    sidebar: [
+      {
+        title: '句子',
+        path: '/sentence.md'
+      },
+      {
+        title: '词汇',
+        collapsable: false,
+        children: [
+          {
+            title: '按字母分类',
+            path: '/vocabulary/by-first-letter'
+          },
+          {
+            title: '按场景分类',
+            path: '/vocabulary/by-scence'
+          },
+        ]
+      },
+    ]
+  }
+}
---
+++ progress/dudu/docs/sentence.md
@@ -0,0 +1,13 @@
+# 句子
+## What's your name? I'm...
+What's your name?  
+I'm Dudu. My name is Dudu.
+
+## What is it? It's ...
+What is it?  
+It's a car.
+
+## What do you see? I see ...
+I see a cat.  
+I see a dog.
+
